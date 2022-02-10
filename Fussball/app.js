d60699b90268f7d83a8e106cef3185a16269e714
noahlauper <noahlauper@gmail.com>
2022-02-10 16:13:20 +0100

    Es wird schön dargestellt was die nächsten Spiele sind

--- Fussball/app.js
+++ Fussball/app.js
@@ -1,4 +1,3 @@
-
 fetch('https://api.football-data.org/v2/teams/86/matches?status=SCHEDULED', {
     headers: { 'X-Auth-Token': '4d111a4bf7654a24aa3caac1df58401d' },
     method: 'GET'
@@ -9,22 +8,21 @@ fetch('https://api.football-data.org/v2/teams/86/matches?status=SCHEDULED', {
 })
 
 .then(data => {
-    console.log(data.matches)
-    const html = data.matches.map (matches =>{
-        return `<p>Away Team: ${JSON.stringify(matches.awayTeam.name)}</p>`
+    const html = data.matches
+    .map (matches =>{
+        return `
+        <div class ="Team">
+            <p>
+                Home Team: ${JSON.stringify(matches.homeTeam.name)}</br>
+                Away Team: ${JSON.stringify(matches.awayTeam.name)} 
+            </p>
+        </div> 
+        `
     });
-    console.log(html)
     document.querySelector('#app').innerHTML= html
+    .join("")
 })
 
-.then(data => {
-    console.log(data.matches)
-
-    const html2 = data.matches.map (matches =>{
-        return `<p>Home Team: ${JSON.stringify(matches.homeTeam.name)}</p>`
-    })
-    document.querySelector('#app2').innerHTML= html2
-})
 
 .catch(function (err) {
     console.log('error: ' + err);
@@ -32,25 +30,3 @@ fetch('https://api.football-data.org/v2/teams/86/matches?status=SCHEDULED', {
 
 
 
-/*
-function appendData(data) {
-var mainContainer = document.getElementById("myData");
-for (var i = 0; i < data.length; i++) {
-    var div = document.createElement("div");
-    div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
-    mainContainer.appendChild(div);
-}
-}
-.then((Json) => {
-    console.log(Json)
-})*/
-
-/*
-.then(function (response) {
-    console.log(response);
-    appendData(data);
-    return response.json();
-})
-document.getElementById("myData").innerHTML = 'response';
-
-*/
