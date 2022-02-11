7202849e885739ae7efde2552fc29774be7f1a3f
Jonas Herth <jonasherth@gmx.ch>
2022-02-11 09:20:46 +0100

    API auf onclick function abrufen

--- Fussball/app.js
+++ Fussball/app.js
@@ -1,33 +1,37 @@
-fetch('https://api.football-data.org/v2/competitions/2000/', {
-    headers: { 'X-Auth-Token': '4d111a4bf7654a24aa3caac1df58401d' },
-    method: 'GET'
-})
+function fetchWinner(winnerId) {
+    fetch('https://api.football-data.org/v2/competitions/' + winnerId + "/", {
+        headers: { 'X-Auth-Token': '231ac5e64da74b0da63a430b667d14a0' },
+        method: 'GET'
+    })
+        .then((response) => {
+            return response.json();
+        })
 
-.then((response) => {
-    return response.json();
-})
-
-.then(data => {
-    console.log(data.seasons)
-    const html = data.seasons
-    .map (seasons =>{
-        return `
+        .then(data => {
+            console.log(data.seasons)
+            const html = data.seasons
+                .map(seasons => {
+                    return `
         <div class ="data">
         <p>
-            Winner: ${JSON.stringify(seasons.winner.name)}</br>
+            Winner: ${JSON.stringify(seasons.winner?.name)}</br>
             Date: ${JSON.stringify(seasons.endDate)}
         </p>
         </div>
         `
-    });
-    console.log(html)
-    document.querySelector('#app').innerHTML= html
-    .join("")
-})
+                });
+            console.log(html)
+            document.querySelector('#app').innerHTML = html
+                .join("")
+        })
 
-.catch(function (err) {
-    console.log('error: ' + err);
-});
+        .catch(function (err) {
+            console.log('error: ' + err);
+        }
+        )}
 
 
+//CL 2001
+//2012
+//PL 2021
 
